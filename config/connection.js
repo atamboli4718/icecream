cb3467426a0426e940ff3c90dc24a04ee20120a8
Alicia Tamboli <atamboli4718@gmail.com>
2019-07-28 15:46:11 -0400

    troubleshooting heroku deployment, tweaked connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 var mysql = require("mysql");
+var connection; 
+
 if (process.env.JAWSDB_URL){
-  connection.mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "432ograL!",
     database: "icecream_db"  
   }
-);
+)};
 
 connection.connect(function(err) {
   if (err) {
@@ -19,4 +20,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
